b2f59ac30b086d13389ddd335f5a46a8212f19ee
Jugal Kishore Palli <drjkpalli@gmail.com>
2024-07-01 14:42:51 +0530

    Added cloudflare tunnel to script.js

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ const sideContent = document.querySelector(".side-content");
 const modeSwitch = body.querySelector(".mode-button");
 const mainContent = document.querySelector(".main-content");
 const navLinks = document.querySelectorAll(".nav-links a");
-const backend = "http://localhost:8000/";
+const backend = "https://knives-openings-growing-accept.trycloudflare.com/";
 let activeItem = null;
 let activeSection = null;
 
