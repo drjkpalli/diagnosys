b29d7b14a9610ac4937c13b14b3c3dd15de05d4a
Jugal Kishore Palli <drjkpalli@gmail.com>
2024-07-02 12:19:52 +0530

    Tunnel Update script.js

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ const sideContent = document.querySelector(".side-content");
 const modeSwitch = body.querySelector(".mode-button");
 const mainContent = document.querySelector(".main-content");
 const navLinks = document.querySelectorAll(".nav-links a");
-const backend = "https://inch-losses-organ-desktop.trycloudflare.com/";
+const backend = "https://dual-particular-finding-farmer.trycloudflare.com/";
 let activeItem = null;
 let activeSection = null;
 
